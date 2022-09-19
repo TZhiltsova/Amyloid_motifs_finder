93ddeb1fc13c70884e6c2f72fa9311b2b6096373
TZhiltsova <t.y.zhiltsova@mail.ru>
2022-09-19 13:32:12 +0300

    Nothing new in the code, just adding fasta file of disprot peptides and sheets with amilod-forming sequences from Waltz DB

--- Amiloids-Disprot.py
+++ Amiloids-Disprot.py
@@ -1,8 +1,12 @@
 from pyfaidx import Fasta
-genes = Fasta('/home/lora/Amilods/Disprot/Amiloids-Disprot/DisProt_2022_06.fasta')
-seq_1000 = []
+
+genes = Fasta('/home/lora/Amilods/Disprot/Amiloids-Disprot/DisProt_2022_06.fasta', filt_function = lambda x: len[key] <= 1000)
+seq_1000 = []  # list for all sequences which are shorter than 1000 residues
+print(genes[9][:])
+'''
 for key in genes:
     if len(key) < 1000:
         seq_1000.append(key)
-for key in seq_1000:
-    print(key)
+    for key in seq_1000:
+        print(key)
+'''
