9674ea3e9aad5546f32ba120dc5a4ef8bbce5504
TZhiltsova <t.y.zhiltsova@mail.ru>
2022-09-20 18:36:56 +0300

    - Add fasta to .gitignore - Update to the code: find the way to delete sequences longer then 1000 ac

--- Amiloids-Disprot.py
+++ Amiloids-Disprot.py
@@ -1,12 +1,11 @@
 from pyfaidx import Fasta
 
-genes = Fasta('/home/lora/Amilods/Disprot/Amiloids-Disprot/DisProt_2022_06.fasta', filt_function = lambda x: len[key] <= 1000)
-seq_1000 = []  # list for all sequences which are shorter than 1000 residues
-print(genes[9][:])
-'''
-for key in genes:
-    if len(key) < 1000:
-        seq_1000.append(key)
-    for key in seq_1000:
-        print(key)
-'''
+genes = Fasta('/home/lora/Amilods/Disprot/Amiloids-Disprot/DisProt_2022_06.fasta')
+seq_1000 = dict()  # list for all sequences which are shorter than 1000 residues
+i = 0 # counter for number of peptide in fasta file
+while i < len(genes.keys()):
+    if genes[i][:].end < 1000:                       # deleting all sequences longer then 1000 amino acids
+        seq_1000[genes[i][:].name] = genes[i][:]
+    i += 1
+for key in seq_1000:
+    print(key, seq_1000[key])
