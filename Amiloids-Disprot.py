013f7b82bdbf484a2b1e8c47216dcc1064229a1d
TZhiltsova <t.y.zhiltsova@mail.ru>
2022-11-17 21:13:57 +0300

    This script is able to find amiloid regions and create correct fasta file

--- Amiloids-Disprot.py
+++ Amiloids-Disprot.py
@@ -12,15 +12,35 @@ with open('WALTZ_DB_amiloid_seq') as waltz_db:
             break
         else:
             waltz_dict[i] = count
+
 seq_1000 = dict()  # dict for all sequences which are shorter than 1000 residues
 filter_seq = dict()  # dict for seq that have amiloid-forming region
 with Fasta('DisProt_2022_06.fasta') as genes:
     for i in range((len(genes.keys()))):
         if genes[i][:].end < 1000:
             seq_1000[genes[i][:].name] = genes[i][:]
+
 for key, seq in seq_1000.items():
     for val in waltz_dict.values():
         if val in str(seq):
+            #if str(seq) not in filter_seq.values():
             filter_seq[key] = seq
+
+discript = []
+with Fasta('DisProt_2022_06.fasta') as genes:
+    for record in genes:
+        line = record.long_name
+        discript.append(line[:])
+
+amiloid_seq = {}
 for key, val in filter_seq.items():
-    print(key, val)
+    for disc in discript:
+        if key in disc:
+            amiloid_seq[disc] = val
+
+
+with open('Amiloid+Disprot_full_corr', 'w') as A_D:
+    for key, val in amiloid_seq.items():
+        A_D.write(key + '\n')
+        A_D.write(str(val) + '\n')
+        A_D.write('\n')
