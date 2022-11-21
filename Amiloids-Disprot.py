e76edb313b050cae00e56411ae8aa73ed0cb4632
TZhiltsova <t.y.zhiltsova@mail.ru>
2022-11-21 20:34:08 +0300

    In this update amiloid sequences were added to a head of fasta file

--- Amiloids-Disprot.py
+++ Amiloids-Disprot.py
@@ -20,27 +20,46 @@ with Fasta('DisProt_2022_06.fasta') as genes:
         if genes[i][:].end < 1000:
             seq_1000[genes[i][:].name] = genes[i][:]
 
+short_amiloid_list_for_head = {}
 for key, seq in seq_1000.items():
+    short_amiloid_list = []
     for val in waltz_dict.values():
         if val in str(seq):
             #if str(seq) not in filter_seq.values():
             filter_seq[key] = seq
+            short_amiloid_list.append(val)
+            short_amiloid_list_for_head[key] = short_amiloid_list
 
-discript = []
+discriptor = []
 with Fasta('DisProt_2022_06.fasta') as genes:
     for record in genes:
         line = record.long_name
-        discript.append(line[:])
+        discriptor.append(line[:])
 
-amiloid_seq = {}
+amiloid_seq_no_amil_disc = {}
 for key, val in filter_seq.items():
-    for disc in discript:
+    for disc in discriptor:
         if key in disc:
-            amiloid_seq[disc] = val
+            amiloid_seq_no_amil_disc[disc] = val
+
+amil_seq = {}
+for key, val in amiloid_seq_no_amil_disc.items():
+    head_for_fasta = key
+    for key_amil, val_amil in short_amiloid_list_for_head.items():
+        if key_amil in key:
+            for i in val_amil:
+                if ' amilod_seq=' not in head_for_fasta:
+                    head_for_fasta += ' amilod_seq=' + i
+                else:
+                    head_for_fasta += ', ' + i
+    amil_seq[head_for_fasta] = val
+
+amil_seq_no_repeats = {}
+for key, val in amil_seq.items():
+    amil_seq_no_repeats[str(val)] = key
 
 
 with open('Amiloid+Disprot_full_corr', 'w') as A_D:
-    for key, val in amiloid_seq.items():
-        A_D.write(key + '\n')
+    for key, val in amil_seq_no_repeats.items():
         A_D.write(str(val) + '\n')
-        A_D.write('\n')
+        A_D.write(key + '\n')
