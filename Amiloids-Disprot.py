c1ad886442a35d5610bf24d9c4474fde10618cf7
TZhiltsova <t.y.zhiltsova@mail.ru>
2022-11-28 01:53:09 +0300

    Draft

--- Amiloids-Disprot.py
+++ Amiloids-Disprot.py
@@ -1,5 +1,5 @@
 from pyfaidx import Fasta
-
+import csv
 
 waltz_seq = []
 waltz_dict = {}    #dictionry for sequences from WaltzDB
@@ -20,6 +20,17 @@ with Fasta('DisProt_2022_06.fasta') as genes:
         if genes[i][:].end < 1000:
             seq_1000[genes[i][:].name] = genes[i][:]
 
+tsv_list = {}
+count = 0
+with open('DisProt release_2022_06.tsv') as tsv:
+    file = csv.reader(tsv, delimiter='\t')
+    for rows in file:
+        while '' in rows:
+            rows.remove('')
+        for i in range(0, len(rows)):
+            tsv_list[rows[len(rows)-1]] = rows[0]
+
+new_tsv = {}
 short_amiloid_list_for_head = {}
 for key, seq in seq_1000.items():
     short_amiloid_list = []
@@ -29,6 +40,26 @@ for key, seq in seq_1000.items():
             filter_seq[key] = seq
             short_amiloid_list.append(val)
             short_amiloid_list_for_head[key] = short_amiloid_list
+            for pep, uni in tsv_list.items():
+                if str(seq) in pep:
+                    new_tsv[pep] = uni
+
+delet_repeats = []
+for key, seq in filter_seq.items():
+    delet_repeats.append([key, str(seq)])
+
+delet_repeats2 = []
+for couples in delet_repeats:
+    count = 0
+    count2 = 0
+    for coup in delet_repeats2:
+        if couples[1] in coup[1]:
+            count += 1
+        elif coup[1] in couples[1]:
+            count2 += 1
+    if count == 0:
+        delet_repeats2.append(couples)
+
 
 discriptor = []
 with Fasta('DisProt_2022_06.fasta') as genes:
@@ -43,23 +74,63 @@ for key, val in filter_seq.items():
             amiloid_seq_no_amil_disc[disc] = val
 
 amil_seq = {}
+fasta_uniprot = {}
 for key, val in amiloid_seq_no_amil_disc.items():
+    head_for_fasta_new = key
     head_for_fasta = key
+    amils_for_uniprot = ''
+    uni_for_uniprot = ''
+    pep_for_uniprot = ''
+    for pep, uni in new_tsv.items():
+        if str(val) in pep:
+            head_for_fasta_new = '>UniProt|' + uni + ' ' + head_for_fasta.replace('>', '')
+            uni_for_uniprot = '>UniProt|' + uni
+            pep_for_uniprot = pep
     for key_amil, val_amil in short_amiloid_list_for_head.items():
         if key_amil in key:
             for i in val_amil:
-                if ' amilod_seq=' not in head_for_fasta:
-                    head_for_fasta += ' amilod_seq=' + i
+                if ' amilod_seq=' not in head_for_fasta_new:
+                    head_for_fasta_new += ' amilod_seq=' + i
                 else:
-                    head_for_fasta += ', ' + i
-    amil_seq[head_for_fasta] = val
+                    head_for_fasta_new += ', ' + i
+                if ' amilod_seq=' not in uni_for_uniprot:
+                    uni_for_uniprot += ' amilod_seq=' + i
+                else:
+                    uni_for_uniprot += ', ' + i
+    fasta_uniprot[pep_for_uniprot] = uni_for_uniprot
+    amil_seq[head_for_fasta_new] = val
+
+'''
+fasta_uniprot_no_repeats = {}
+for pep, uni in fasta_uniprot.items():
+    fasta_uniprot_no_repeats[uni] = pep
+for uni, pep in fasta_uniprot_no_repeats.items():
+    print(uni, pep, sep='\n')
 
 amil_seq_no_repeats = {}
 for key, val in amil_seq.items():
     amil_seq_no_repeats[str(val)] = key
+'''
 
+table = []
+with open('DisProt release_2022_06.tsv') as tsv:
+    file = csv.reader(tsv, delimiter='\t')
+    count = 0
+    for rows in file:
+        if count == 0:
+            table.append(rows)
+            count += 1
+        for pep, uni in new_tsv.items():
+            if uni in rows and pep in rows:
+                table.append(rows)
+
+with open('table_amiloids.tsv', 'w') as tab:
+    for elem in table:
+        for words in elem:
+            tab.write(words + ', ')
+        tab.write('\n')
 
 with open('Amiloid+Disprot_full_corr', 'w') as A_D:
-    for key, val in amil_seq_no_repeats.items():
+    for key, val in amil_seq.items():
         A_D.write(str(val) + '\n')
         A_D.write(key + '\n')
